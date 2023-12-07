8a99096105febd38bcd7e01ea660f8add26bb7ee
dttrue <danieltorres@pursuit.org>
2023-12-07 12:55:17 -0500

    Daniels Implement component composition with class components

---
+++ jsx-basic-react-app/src/ClassFarewell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ClassFarewell extends React.Component {
+    render() {
+        return <div>
+            <h1>Goodbye from Class Component</h1>
+        </div>;
+    }
+}
+
+export default ClassFarewell
