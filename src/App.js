1eb1c230131f290767fea7c04489e465959bf728
MoniqueC10-4 <monique.charles13@gmail.com>
2023-12-20 22:48:25 -0500

    Implement component composition with class components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ClassFarewell from './ClassFarewell.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">   
+      <ClassFarewell />
+    </div>    
+  );
+}
+
+export default App;
