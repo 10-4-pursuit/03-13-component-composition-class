2833631e5f522264b87dfda67d4fa870ea336e94
bperalta14 <briannaperalta@pursuit.org>
2024-01-11 14:25:26 -0500

    Implement component composition with class components

---
+++ 03-13-comp-class/src/ClassFarewell.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClassFarewell extends Component {
+    render() {
+        return (
+            <div>
+                Goodbye from Class Component!
+            </div>
+        );
+    };
+}
+
+export default ClassFarewell;
